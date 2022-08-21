55cf2a45cda0e5be2e45672015ff69f680cf2389
smartlycoding <joinalzx@gmail.com>
2022-08-21 09:23:16 +0600

    loop

--- for_loop.js
+++ for_loop.js
@@ -2,7 +2,16 @@
 for (let step = 0; step < 5; step ++){
         console.log('walking west one step');
 }
-*/
+
 for (let i = 0; i < 0; i ++){
     console.log('walking')
 }
+let num;
+do {
+    console.log("Inside the loop");
+    num = prompt("Enter the number");
+} while (num < 10);
+    console.log('Outside the loop');
+
+*/
+
